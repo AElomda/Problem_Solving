8150fa516b42d6cc0fed5f354a14a2a36603ab19
elomd <elomdaa7444@gmail.com>
2023-08-16 00:01:59 +0300

    Greatest Common Divisor of Strings

---
+++ Easy/Greatest Common Divisor of Strings/Greatest Common Divisor of Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "abcabc";
+        string str2 = "abc";
+
+        string result = GcdOfStrings(str1, str2);
+        Console.WriteLine($"Greatest Common Divisor of Strings: {result} ");
+    }
+
+    static string GcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)
+            return "";
+
+        int gcdLength = Gcd(str1.Length, str2.Length);
+        return str1.Substring(0, gcdLength);
+    }
+
+    static int Gcd(int a, int b)
+    {
+        while (b > 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
